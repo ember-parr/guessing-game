4763b41cbe76d6a10061891185bd7f1d0b078222
Ember Parr <ember.devops@gmail.com>
2020-11-16 13:03:23 -0600

    user has four tries

--- Program.cs
+++ Program.cs
@@ -37,7 +37,7 @@ namespace GuessingGame
  .      -.   . . . .: .:::.:.\.           .:/
 .   .   .  :      : ....::_:..:\   ___.  :/    Hello Human,
    .   .  .   .:. .. .  .: :.:.:\       :/      Welcome to my guessing game...
-     +   .   .   : . ::. :.:. .:.|\  .:/|
+     +   .   .   : . ::. :.:. .:.|\  .:/|           You have 4 guesses    
      .         +   .  .  ...:: ..|  --.:|
 .      . . .   .  .  . ... :..:..'(  ..)'
  .   .       .      :  .   .: ::/  .  .::\ 
@@ -48,24 +48,47 @@ namespace GuessingGame
 
         static void EdwardAsks()
         {
-            Console.Write("Guess a number: ");
-            string response = Console.ReadLine().ToLower();
-            int secretNumber = 42;
-
-            if (secretNumber == int.Parse(response))
+            int attempt = 1;
+            while (attempt < 5)
             {
-                Console.WriteLine("Nicely done!");
+
+                Console.Write($"[try {attempt}] Guess a number: ");
+                string response = Console.ReadLine().ToLower();
+                int secretNumber = 42;
+
+                if (secretNumber == int.Parse(response))
+                {
+                    Console.WriteLine("Nicely done!");
+                    attempt = 10;
+                    EdwardCelebrates();
+                }
+                else
+                {
+                    Console.WriteLine("WRONG SUCKAH!");
+                    attempt++;
+                }
             }
-            else
-            {
-                Console.WriteLine("WRONG SUCKAH!");
-            }
+        }
 
-
-
-
-
-
+        static void EdwardCelebrates()
+        {
+            Console.WriteLine(@"
+     /    \ _| /
+     q .. p \ /
+      \--/  //
+     __||__//
+    /.    _/
+   // \  /                 you got it!
+  //   ||
+  \\  /  \
+   )\|    |
+  / || || |
+  |/\| || |
+     | || |
+     \ || /
+   __/ || \__
+  \____/\____/
+            ");
         }
     }
 
